55ae158395748c00d4f5bd116e562f77e4abc6be
jjaranda13 <jose_javier.garcia_aranda@alcatel-lucent.com>
2015-07-20 12:54:56 +0200

    Update util.py

--- cluster/util.py
+++ cluster/util.py
@@ -14,6 +14,8 @@
 # along with this library; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
+# new functions HDcentroid() by:
+#    2015 Jose Javier Garcia Aranda , Juan Ramos Diaz
 
 from __future__ import print_function
 import logging
